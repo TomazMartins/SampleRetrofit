8e217b003f49f4acdbb3a54309fcc8deb24b249a
TomazMartins <tomaz.r.martins@gmail.com>
2017-04-23 14:30:22 -0300

    Implementing PokemonStat and Stat entities
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

---
+++ app/src/main/java/br/com/samples/tomazmartins/sampleretrofit/model/PokemonStats.java
@@ -0,0 +1,35 @@
+package br.com.samples.tomazmartins.sampleretrofit.model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+class PokemonStats {
+    @SerializedName( "stat" ) private Stat mStat;
+    @SerializedName( "effort" ) private int mEffort;
+    @SerializedName( "base_stat" ) private int mBaseStat;
+
+
+    public Stat getStat() {
+        return mStat;
+    }
+
+    public void setStat( Stat stat ) {
+        this.mStat = stat;
+    }
+
+    public int getEffort() {
+        return mEffort;
+    }
+
+    public void setEffort( int effort ) {
+        this.mEffort = effort;
+    }
+
+    public int getBaseStat() {
+        return mBaseStat;
+    }
+
+    public void setBaseStat( int baseStat ) {
+        this.mBaseStat = baseStat;
+    }
+}
---
+++ app/src/main/java/br/com/samples/tomazmartins/sampleretrofit/model/Stat.java
@@ -0,0 +1,26 @@
+package br.com.samples.tomazmartins.sampleretrofit.model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+class Stat {
+    @SerializedName( "id" ) private int mId;
+    @SerializedName( "name" ) private String mName;
+
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId( int id ) {
+        this.mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName( String name ) {
+        this.mName = name;
+    }
+}
