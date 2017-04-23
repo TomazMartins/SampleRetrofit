17a6d88fd10f625126c64cdbbf1521df89d2cacf
TomazMartins <tomaz.r.martins@gmail.com>
2017-04-23 14:33:38 -0300

    Implementing PokemonType and Type entities
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

---
+++ app/src/main/java/br/com/samples/tomazmartins/sampleretrofit/model/PokemonType.java
@@ -0,0 +1,26 @@
+package br.com.samples.tomazmartins.sampleretrofit.model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+class PokemonType {
+    @SerializedName( "slot" ) private int mSlot;
+    @SerializedName( "type" ) private Type mType;
+
+
+    public int getSlot() {
+        return mSlot;
+    }
+
+    public void setSlot( int slot ) {
+        this.mSlot = slot;
+    }
+
+    public Type getType() {
+        return mType;
+    }
+
+    public void setType( Type type ) {
+        this.mType = type;
+    }
+}
---
+++ app/src/main/java/br/com/samples/tomazmartins/sampleretrofit/model/Type.java
@@ -0,0 +1,26 @@
+package br.com.samples.tomazmartins.sampleretrofit.model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+class Type {
+    @SerializedName( "mId" ) private int mId;
+    @SerializedName( "mName" ) private String mName;
+
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId( int id ) {
+        this.mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName( String name ) {
+        this.mName = name;
+    }
+}
